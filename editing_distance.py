ecdb7bdb2344598ce6efe9723630dc5d2bf4e79d
Nikola Kalábová <nikola@kalabova.eu>
2021-05-18 13:49:26 +0200

    Delete editing_distance.py

--- editing_distance.py
+++
@@ -1,7 +0,0 @@
-from Bio import pairwise2
-from Bio.pairwise2 import format_alignment
-import sys
-
-for a in pairwise2.align.globalms(sys.argv[1], sys.argv[2], 0, -1, -1, -1):
-    print("editing distance:" + str((-1)*int(a[2])))
-    print("final sequences:\n" + a[0] + "\n" + a[1] + "\n")
